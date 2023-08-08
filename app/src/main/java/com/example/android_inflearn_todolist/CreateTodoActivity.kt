b8bcd1951622ab10a4309d2a49f98f6ad14d17a6
LeeChoongHwan <choo901@naver.com>
2023-08-08 12:05:27 +0900

    item todo

--- app/src/main/java/com/example/android_inflearn_todolist/CreateTodoActivity.kt
+++ app/src/main/java/com/example/android_inflearn_todolist/CreateTodoActivity.kt
@@ -2,10 +2,37 @@ package com.example.android_inflearn_todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_inflearn_todolist.databinding.ActivityCreateTodoBinding
+import com.example.android_inflearn_todolist.db.AppDatabase
+import com.example.android_inflearn_todolist.db.TodoDao
+import com.example.android_inflearn_todolist.db.TodoEntity
 
 class CreateTodoActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCreateTodoBinding
+
+    private lateinit var db : AppDatabase
+    private lateinit var todoDao: TodoDao
+    private lateinit var todoList : ArrayList<TodoEntity>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_todo)
+        binding = ActivityCreateTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+    }
+
+    private fun getAllTodoList() {
+        Thread {
+            todoList = ArrayList(todoDao.getAllTodo())
+            setRecyclerView()
+        }
+    }
+
+    private fun setRecyclerView() {
+
     }
 }
